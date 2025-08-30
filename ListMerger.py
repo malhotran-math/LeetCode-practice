a8b5d21924a0e101d8d3ea0861eec2f33b7f875b
malhotran-math <malhotra.nandan1409@gmail.com>
2025-08-30 21:25:13 +0200

    Update ListMerger.py

--- ListMerger.py
+++ ListMerger.py
@@ -13,3 +13,19 @@ class Solution:
                 nums1[k] = nums2[j]
                 j -= 1
             k -= 1
+
+
+'''
+the above is O(m+n). The following is O((m+n)log(m+n)) 
+
+import numpy as np
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+        for i in np.arange(m,m+n):
+            nums1[i]=num2[i-m] 
+        nums1.sort()
+
+'''
