e704d94bbf2318dbfcb532f74731b0a7dd6b913a
malhotran-math <malhotra.nandan1409@gmail.com>
2025-09-12 13:12:13 +0200

    Update Search2DMatrix.py

--- Search2DMatrix.py
+++ Search2DMatrix.py
@@ -1,7 +1,22 @@
 import numpy as np
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
-        mat = np.array(matrix)
-        mat.flatten()
-        check = target in mat
-        return check
+        # A naive approach, runs in O(m*n) complexity
+        #mat = np.array(matrix)
+        #mat.flatten()
+        #check = target in mat
+        #return check
+
+        # The next approach is O(log(m*n)), but assumes the matrix is sorted
+        m, n = len(matrix), len(matrix[0]) #rows and columns 
+        start, end = 0, m*n-1 #ends for binary search 
+        while start <= end: 
+            mid = (start+end)//2 
+            midval = matrix[mid//n][mid % n]
+            if target == midval :#we don't flatten the matrix, just search row wise 
+                return True 
+            elif target > midval: 
+                start = mid #search the second half
+            else:
+                end = mid #search the first half
+        return False
