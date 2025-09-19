9abfa0d9d592013c5513d25350c9824e56a83d6b
malhotran-math <malhotra.nandan1409@gmail.com>
2025-09-19 10:48:57 +0200

    Update JumpGame.py

--- JumpGame.py
+++ JumpGame.py
@@ -6,3 +6,6 @@ class Solution:
                 return False
             reach = max(reach, pos + val)
         return True
+
+#This is a greedy algorithm approach. It does not care if your jumps are optimal, but the algorithm is efficient enough, running in O(n)
+#time where n = len(nums). 
